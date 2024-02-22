a882b1d2f4e79c194a49d3f4ae1323e311e94a31
breadbaker7723 <breadbaker7723@gmail.com>
2024-02-22 18:16:51 -0500

    Working Example Data

--- Example_Usage_and_Results/2022_Snow_YN.py
+++ Example_Usage_and_Results/2022_Snow_YN.py
@@ -30,43 +30,35 @@ def timeTemp(listName):
     timeTemps=[]
     timeTempsWithYear=[]
     for i in range(len(listName)): #-1?
-    #for i in range(5):
         noEnd=listName[i].replace(",,,,,","")
         timeList=noEnd.split(",")
-        print(timeList)
-        if len(timeList[1]) < 2:
+        #print(timeList)
+        if len(timeList) < 2 or len(timeList[1]) < 2:
             return timeTemps, timeTempsWithYear
         if timeList[1].startswith("0") == False and timeList[1][1] == "/":
             timeList[1]="0"+timeList[1]
-            #print("first if statement complete")
-            #print(timeList)
         #print(timeList[1])
         if timeList[1][3] != "0" and timeList[1][4] == "/":
             timeList[1]=timeList[1][0:3]+"0"+timeList[1][3:]
-            #print("Second if statement complete")
-            #print(timeList)
         #print(timeList[1])
         if timeList[1][6]=="2":
-            slashyear=timeList[1][5:10]
+            slashyear=timeList[1][5:8]
             realslashyear=slashyear[0]+slashyear[3:]
-            #print(slashyear)
+            #print('1', slashyear)
             timeList[1]=timeList[1].replace(slashyear,"")
-            #print("Third if statement complete")
-            #print(timeList)
         elif timeList[1][5]=="/":
             slashyear=timeList[1][5:8]
             realslashyear=slashyear
-            #print(slashyear)
+            #print("2", slashyear)
             timeList[1]=timeList[1].replace(slashyear,"")
-            #print("Fourth if statement complete")
-        if timeList[1].endswith("3:00 AM")==True or timeList[1].endswith("3:00:00 AM")==True or timeList[1].endswith("3:00")==True:
-            timeList[1]=timeList[1][0:6]+"03:00"
-        if timeList[1].endswith("9:00 AM")==True or timeList[1].endswith("9:00:00 AM")==True or timeList[1].endswith("9:00")==True:
-            timeList[1]=timeList[1][0:6]+"09:00"
-        if timeList[1].endswith("3:00 PM")==True or timeList[1].endswith("3:00:00 PM")==True or timeList[1].endswith("15:00:00")==True:
-            timeList[1]=timeList[1][0:6]+"15:00"
-        if timeList[1].endswith("9:00 PM")==True or timeList[1].endswith("9:00:00 PM")==True or timeList[1].endswith("21:00:00")==True:
-            timeList[1]=timeList[1][0:6]+"21:00"
+        if timeList[1].endswith("12:36 AM")==True or timeList[1].endswith("12:36:32 AM")==True or timeList[1].endswith("0:36")==True:
+            timeList[1]=timeList[1][0:6]+"00:36"
+        if timeList[1].endswith("6:36 AM")==True or timeList[1].endswith("6:36:32 AM")==True or timeList[1].endswith("6:36")==True:
+            timeList[1]=timeList[1][0:6]+"06:36"
+        if timeList[1].endswith("12:36 PM")==True or timeList[1].endswith("12:36:32 PM")==True or timeList[1].endswith("12:36")==True:
+            timeList[1]=timeList[1][0:6]+"12:36"
+        if timeList[1].endswith("6:36 PM")==True or timeList[1].endswith("6:36:32 PM")==True or timeList[1].endswith("18:36")==True:
+            timeList[1]=timeList[1][0:6]+"18:36"
         #print(timeList[1])
         #print(timeList[1])
         timeTemp=[]
@@ -87,14 +79,12 @@ def dateIndex(listName):
         onlyDate=listName[i][0]
         datesOnly.append(onlyDate)
     #print(datesOnly)
-    MarTwenty=datesOnly.index('03/21 03:00')
-    JanFirst=datesOnly.index('01/01 03:00')
-    OctFirst=datesOnly.index('10/01 03:00')
+    MarTwenty=datesOnly.index('03/21 00:36')
+    JanFirst=datesOnly.index('01/01 00:36')
+    OctFirst=datesOnly.index('10/16 00:36')
     idxList=[MarTwenty,JanFirst,OctFirst]
-    print(MarTwenty)
-    print(JanFirst)
-    print(OctFirst)
-    print(idxList)
+    #print(MarTwenty)
+    #print(JanFirst)
     return idxList
 
 def snowNoSnow(listName,MarTwenty):
@@ -103,12 +93,8 @@ def snowNoSnow(listName,MarTwenty):
     # print(listNew)
     for i in range(len(listNew)):
         BackSnow=True
-        #print("hello there")
-        #print(listNew[i][1])
-        #print("general greiviousi")
+        #print(listNew[i])
         if float(listNew[i][1]) <= 1:
-            #print(listNew[i+1][1])
-            #print(listNew[i-1][1])
             for j in range(4):
                 if float(listNew[i-j][1]) > 1:
                     BackSnow=False
@@ -123,23 +109,23 @@ def snowNoSnow(listName,MarTwenty):
     ForwardZeroOne=[]
     for i in range(len(listNew)):
         ForwardSnow=True
-        #print("hello there")
-        #print(listNew[i][1])
-        #print("general greiviousi")
-        if float(listNew[i][1]) <= 1:
-            #print(listNew[i+1][1])
-            #print(listNew[i-1][1])
-            for j in range(4):
-                if float(listNew[i+j][1]) > 1:
-                    ForwardSnow=False
-            if ForwardSnow==True:
-                ForwardZeroOne.append(1)
-                #print("True")
+        #print(listNew[i])
+        if listNew[i][0][6:8] == "16":
+            ForwardZeroOne.append(2016)
+        else:
+            if float(listNew[i][1]) <= 1:
+            #print("VALUE LESS THAN 1")
+                for j in range(4):
+                    if float(listNew[i+j][1]) > 1:
+                        ForwardSnow=False
+                if ForwardSnow==True:
+                    ForwardZeroOne.append(1)
+                    #print("True")
+                else:
+                    ForwardZeroOne.append(0)
+                    #print("False")
             else:
                 ForwardZeroOne.append(0)
-                #print("False")
-        else:
-            ForwardZeroOne.append(0)
     FinalZeroOne = []
     for i in range(len(listNew)):
         if ForwardZeroOne[i] == 1 or BackZeroOne[i] == 1:
@@ -177,27 +163,28 @@ def main(fileName):
     MarTwenty=dateIdx[0]
     JanFirst=dateIdx[1]
     OctFirst=dateIdx[2]
-    FinalZeroOne=snowNoSnow(timeTempsWithYear,OctFirst)[0]
+    ZeroOne=snowNoSnow(timeTempsWithYear,OctFirst)[0]
     timeTempsinRange=snowNoSnow(timeTempsWithYear,OctFirst)[1]
     #print(ZeroOne)
     timesYN=[]
     for i in range(len(timeTempsinRange)):
         timeYN=[]
         timeYN.append(timeTempsinRange[i][0])
-        timeYN.append(FinalZeroOne[i])
+        timeYN.append(ZeroOne[i])
         timesYN.append(timeYN)
     #print(timesYN)
     print("----------"+filenocsv+"----------")
     NineList = []
     for i in range(len(timesYN)):
-        print(str(timesYN[i][0])+","+str(timesYN[i][1]))
-        if timesYN[i][0].endswith("9:00")==True:
+        #print(str(timesYN[i][0])+","+str(timesYN[i][1]))
+        #print(timesYN[i])
+        if timesYN[i][0].endswith("06:36")==True:
             NineList.append(timesYN[i][1])
-        if timesYN[i][0].endswith("03/20/13 15:00"):
+        if timesYN[i][0].endswith("03/21/ 00:36"):
             NineList.append("This is the beginning of the snow season")
-        if timesYN[i][0].endswith("06/30/13 15:00"):
+        if timesYN[i][0].endswith("06/30/ 12:36"):
             NineList.append("This is the end of the snow season")
-    #print("9:00 YN Values for File: " + filenocsv)
+    print("9:00 YN Values for File: " + filenocsv)
     print(*NineList, sep="\n")
 
 for file in fileList:
